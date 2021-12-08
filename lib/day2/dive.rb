6ff0f5dfe8d0dce032d7f3bb01d497466529e9f7
abreaux26 <angelbreaux26@gmail.com>
2021-12-08 08:55:43 -0600

    Day 2 Part 2

--- lib/day2/dive.rb
+++ lib/day2/dive.rb
@@ -1,46 +1,39 @@
-module Dive
-  extend self
+class Dive
+  attr_accessor :aim, :depth, :horizontal
 
-  def depth_total
-    down_total = sum_of_units(depth_down_data)
-    up_total = sum_of_units(depth_up_data)
-
-    (down_total - up_total).abs
+  def initialize
+    @aim = 0
+    @depth = 0
+    @horizontal = 0
   end
 
-  def horizontal_total
-    sum_of_units(horizontal_data)
+  def plan_course
+    get_data.each do |command_and_units|
+      command, units = command_and_units.split(' ')
+
+      calculate_units(command, units.to_i)
+    end
   end
 
   def total
-    depth_total * horizontal_total
+    @depth * @horizontal
   end
 
   private
 
-  def sum_of_units(data)
-    data.reduce(0) do |sum, position|
-      sum += position.split(' ')[1].to_i
+  def calculate_units(command, units)
+    case command
+    when "forward"
+      @horizontal += units
+      @depth += @aim * units
+    when "up"
+      @aim -= units
+    when "down"
+      @aim += units
     end
-  end
-
-  def depth_down_data
-    filter_data("down")
-  end
-
-  def depth_up_data
-    filter_data("up")
   end
 
   def get_data
     @_get_data ||= File.read("./lib/day2/puzzle_input.txt").split("\n")
-  end
-
-  def horizontal_data
-    filter_data("forward")
-  end
-
-  def filter_data(command_type)
-    get_data.filter { |command| command.downcase.include? command_type }
   end
 end
--- spec/day2/dive_spec.rb
+++ spec/day2/dive_spec.rb
@@ -2,20 +2,38 @@ require "./lib/day2/dive"
 
 RSpec.describe Dive do
   it "gets horizontal position" do
-    horizontal = Dive.horizontal_total
+    # horizontal = Dive.new.horizontal
+    dive = Dive.new
+    dive.plan_course
 
-    expect(horizontal).to eq 1832
+    expect(dive.horizontal).to eq 1832
+    # expect(dive.horizontal).to eq 15
   end
 
   it "gets depth" do
-    depth = Dive.depth_total
+    # depth = Dive.new.depth
+    dive = Dive.new
+      dive.plan_course
 
-    expect(depth).to eq 1172
+    # expect(dive.depth).to eq 1172 # part 1
+    # expect(dive.depth).to eq 60 # test data
+    expect(dive.depth).to eq 1116059 # part 2
   end
 
   it "gets total by multiplying the horizontal position and the depth" do
-    total = Dive.total
+    dive = Dive.new
+    dive.plan_course
 
-    expect(total).to eq 2147104
+    # expect(dive.total).to eq 2147104 # part 1
+    # expect(dive.total).to eq 900 # test data
+    expect(dive.total).to eq 2044620088 # part 2
+  end
+
+  it "gets aim" do
+    dive = Dive.new
+    dive.plan_course
+
+    # expect(dive.aim).to eq 10 # test data
+    expect(dive.aim).to eq 1172 # part 2
   end
 end
